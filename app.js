207f1b9851e5cfc7e41ebc79ff49d1ee3d07ea66
Keith Webber <keith.e.webber@gmail.com>
2016-12-09 02:38:32 -0500

    loop tune

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 //provision the gpio pins 22 for the led output and 17 for the button input
-var led = require("pi-pins").connect(22);
-var Promise = require('promise');
+var  ledToggle, pressCount;
 
 //set the pin mode,  setting pin 22 as an output and 17 as an input
+var led = require("pi-pins").connect(22);
 led.mode('out');
 
 //set the initial value of the LED to be off.
-led.value(true);
+led.value(false);
 
 
-function sleeplight(ms) {
-   led.value(false);
 
-}
+'use strict';
 
-function main(led) {
-  console.log('Taking a break...');
-  led.value(false);
+const express = require('express');
 
-  setTimeout(sleeplight, 3000);
+// Constants
+const PORT = 80;
 
-  led.value(true);
+// App
+const app = express();
+app.get('/on', function (req, res) {
+	var led = require("pi-pins").connect(22);
+	led.mode('out');
+  	led.value(true);
+  	res.send('on\n');
+});
 
-  console.log('Two second later');
-}
-while (true) {
-main(led);
-}
+app.get('/off', function (req, res) {
+	var led = require("pi-pins").connect(22);
+	led.mode('out');
+  	led.value(false);
+  	res.send('off\n');
+});
+
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
