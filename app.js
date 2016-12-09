0742b9804d8dcfaeb97c207b0085a15005c85b0e
Keith Webber <keith.e.webber@gmail.com>
2016-12-09 01:59:27 -0500

    yeah

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var  ledToggle, pressCount;
 //provision the gpio pins 22 for the led output and 17 for the button input
-var led= require("pi-pins").connect(22),
-    button = require("pi-pins").connect(17);
+var led= require("pi-pins").connect(22);
 
 //set the pin mode,  setting pin 22 as an output and 17 as an input
-button.mode('in');
 led.mode('out');
 
 //set the initial value of the LED to be off.
 ledToggle = false;
-pressCount= 0;
 led.value(true);
 
 
@@ -17,7 +14,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function demo() {
+async function demo(led) {
   console.log('Taking a break...');
   led.value(false);
 
@@ -27,4 +24,4 @@ async function demo() {
   console.log('Two second later');
 }
 
-demo();
+demole(led);
